8b5e57d616908fb8be21b7a8ca83e12287f7ff0b
Praddyota <praddyotahayaran100@gmail.com>
2023-06-20 12:13:49 +0530

    Resume updated

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
 import './Footer.css'
-import { BsBoxArrowUp } from 'react-icons/bs'
 import { AiFillGithub } from 'react-icons/ai'
 import { FaLinkedinIn } from 'react-icons/fa'
 
 const Footer = () => {
   return (
     <footer className='footer'>
-      {/* <a href="#" className='footer__logo'><BsBoxArrowUp /></a> */}
-
+      
       <div className="footer__copyright">
         <small>
           &copy; Praddyota Hayaran All rights reserved
@@ -18,7 +16,7 @@ const Footer = () => {
       <div className="footer__social-links">
         <span><a href="https://www.linkedin.com/in/praddyota-hayaran-245543201/"><FaLinkedinIn /></a></span>
         <span><a href="https://github.com/praddyotahayaran"><AiFillGithub /></a></span>
-        {/* <span><a href=""></a></span> */}
+        
       </div>
     </footer >
   )
